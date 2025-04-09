3f6081cb5fe39c9f775a37c4131a91deb4eb15b2
lijiteo <li-jing.teo@keysight.com>
2025-04-09 16:40:32 +0800

    test poll scm git la

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("LALALA");
+        System.out.println("OMG testing again");
     }
 
     public String getMessage() {
