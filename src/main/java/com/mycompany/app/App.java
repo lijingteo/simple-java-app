efe0bc50e55e7d308c83b887ff93c76dee85cb58
lijiteo <li-jing.teo@keysight.com>
2025-04-10 09:50:25 +0800

    Webhook Test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("OMG testing again");
+        System.out.println("MOdified for Webhook Test!!");
     }
 
     public String getMessage() {
